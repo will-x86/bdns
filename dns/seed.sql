f65ffaac630c2bc8cf4cb8435f292386e43512a5
will-x86 <will@kimbell.uk>
2026-03-13 14:52:01 +0000

    fix seed

--- dns/seed.sql
+++ dns/seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (id, timezone) VALUES ('aabbccdd', 'Europe/London');
-INSERT INTO profiles (user_id, name) VALUES ('aabbccdd', 'test-laptop');
+INSERT OR IGNORE INTO users (id, timezone) VALUES ('aabbccdd', 'Europe/London');
+INSERT OR IGNORE INTO profiles (user_id, name) VALUES ('aabbccdd', 'test-laptop');
 
-INSERT INTO user_category_blocks (user_id, category) VALUES ('aabbccdd', 'unified');
-INSERT INTO user_category_blocks (user_id, category) VALUES ('aabbccdd', 'porn');
+INSERT OR IGNORE INTO user_category_blocks (user_id, category) VALUES ('aabbccdd', 'unified');
+INSERT OR IGNORE INTO user_category_blocks (user_id, category) VALUES ('aabbccdd', 'porn');
 
-INSERT INTO permanent_whitelists (user_id, domain) VALUES ('aabbccdd', 'ads.example.com');
+INSERT OR IGNORE INTO permanent_whitelists (user_id, domain) VALUES ('aabbccdd', 'ads.example.com');
 
-INSERT INTO temporary_whitelists (user_id, domain, expires_at) VALUES ('aabbccdd', 'tracker.example.com', 4070908800);
+INSERT OR IGNORE INTO temporary_whitelists (user_id, domain, expires_at) VALUES ('aabbccdd', 'tracker.example.com', 4070908800);
