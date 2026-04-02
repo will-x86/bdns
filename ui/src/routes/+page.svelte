6fa0b14e9614616cc270e973d90774372a9a417b
will-x86 <will@kimbell.uk>
2026-04-02 10:28:05 +0100

    Init daisy ui

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "./layout.css";
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+
+
+<button class="btn">Button</button>
+
+
+<button class="btn btn-primary">Button</button>
+
